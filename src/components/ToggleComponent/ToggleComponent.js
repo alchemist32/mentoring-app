80e707081fab3f8305dd234cd50cc86a4b10cdec
Urbano Gonzalez <urbanog@bluetrailsoft.com>
2021-10-25 18:56:14 -0500

    Styling toggle component

--- src/components/ToggleComponent/ToggleComponent.js
+++ src/components/ToggleComponent/ToggleComponent.js
@@ -3,13 +3,27 @@ import Context from '../../Context';
 import './ToggleComponent.css';
 
 function ToggleComponent({ active: boolean }) {
-  const { setThemeDark } = React.useContext(Context);
+  const { setThemeDark, themeDark } = React.useContext(Context);
   const changeHandler = (event) => setThemeDark(event.target.checked);
 
   return (
     <div className="toggle-container">
-      <label htmlFor="dark-mode">Set dark mode</label>
-      <input type="checkbox" id="dark-mode" onChange={changeHandler} />
+      <p>Set Dark Theme</p>
+      <label htmlFor="dark-mode">
+        <span
+          className="rail"
+          style={
+            !themeDark
+              ? { backgroundColor: '#333333' }
+              : { backgroundColor: '#fff' }
+          }
+        >
+          <span
+            className={themeDark ? 'pill pill-swipe dark-pill' : 'pill'}
+          ></span>
+        </span>
+      </label>
+      <input type="checkbox" id="dark-mode" onChange={changeHandler} hidden />
     </div>
   );
 }
